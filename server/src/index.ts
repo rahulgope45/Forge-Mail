ca01167e88fece782f2444ec1a8593f2ef776e01
rahulgope45 <rahuleile456@gmail.com>
2026-06-10 23:38:09 +0530

    Changed config

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,11 +4,12 @@ import express, { type Request, type Response } from 'express';
 import cookieParser from 'cookie-parser';
 import {prisma,testDatabaseConnection} from './lib/prisma.js'
 import authRoutes from './routes/auth.routes.js'
+import { connectRedis } from './config/redis.js';
+
 const PORT = 3000;
 const app = express();
 app.use(express.json())
 app.use(cookieParser())
-import { connectRedis } from './config/redis.js';
 
 app.get('/',(req:Request,res:Response)=>{
     res.json({msg:"Server startup"})
